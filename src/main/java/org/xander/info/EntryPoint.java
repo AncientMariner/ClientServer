291021dcd21331e2651d58cd80ab915de839ed21
Ancient_Mariner <xander.lysak@gmail.com>
2012-08-13 22:38:39 +0300

    First Draft

--- src/main/java/org/xander/info/EntryPoint.java
+++ src/main/java/org/xander/info/EntryPoint.java
@@ -1,23 +1,22 @@
 package org.xander.info;
 
 class EntryPoint {
-    public static void main(String[] args){
+    public static void main(String[] args) throws ClassNotFoundException {
 
-        if (args[0].isEmpty()){
+        if(args.length != 0 ){
             System.out.println("Please use either \"client\" or \"server\" " +
                     "parameters to invoke client or server");
-            return;
+            // System.exit(0);
+            if (args[0].equals("client")){
+                InfoClient client = new InfoClient();
+                client.go();
+            } else if (args[0].equals("server")){
+                InfoServer server = new InfoServer();
+                server.go();
+            }
+        } else {
+            System.out.println("Please use either \"client\" or \"server\" " +
+                    "parameters to invoke client or server");
         }
-
-        if (args[0].equals("client")){
-            InfoClient client = new InfoClient();
-            client.go();
-        } else if (args[0].equals("server")){
-            InfoServer server = new InfoServer();
-            server.go();
-        }
-        System.out.println("Please use either \"client\" or \"server\" " +
-                "parameters to invoke client or server");
-
     }
 }
